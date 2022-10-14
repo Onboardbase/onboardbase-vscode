0ca0108cb695be9efa8eccab68182e95ec380a62
Lilit0x <baasisek01@gmail.com>
2022-10-14 17:19:17 +0100

    chore: add command for set config

---
+++ src/commands/set.ts
@@ -0,0 +1,10 @@
+//TODO
+// import { window } from "vscode";
+
+// import ConfigManager from '../config';
+
+// export const set = async () => {
+//   await ConfigManager.init("Login");
+
+//   const configSelect = window.showQuickPick(['API Host', 'Dashboard Host', ''])
+// }
